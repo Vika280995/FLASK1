dd7f1ea198c4490280e559f461f22f2a2b8a2a89
AdminX <Vickky28091995@mail.ru>
2022-10-13 17:10:38 +0300

    5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,7 +13,7 @@ class Article(db.Model):
     intro = db.Column(db.String(300),nullable=False)
     text = db.Column(db.Text,nullable=False)
     notes = db.Column(db.String(500))
-    data = db.Column(db.DateTome,dfault=datetime.utcnow)
+    data = db.Column(db.DateTime,default=datetime.utcnow)
 
 
     def __repr__(self):
@@ -36,10 +36,28 @@ def about():
 def newpage():
     return render_template("newpage.html")
 
+@app.route('/create_article',methods=['POST','GET'])
+def create_article():
+    if request.method=="POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'User page'+name+"-"+ str(id)
+
+        artile = Article(title=title,intro=intro,text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("create_article.html")
+
+#@app.route('/user/<string:name>/<int:id>')
+#def user(name,id):
+ #   return 'User page'+name+"-"+ str(id)
 
 
 if __name__ == '__main__':
