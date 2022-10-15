d3a40eabc2da13f2be8554810c5ddf21ad898a93
AdminX <Vickky28091995@mail.ru>
2022-10-15 21:54:57 +0300

    7

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import sqlite3
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
 class Article(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(100),nullable=False)
-    intro = db.Column(db.String(300),nullable=False)
-    text = db.Column(db.Text,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
     notes = db.Column(db.String(500))
-    data = db.Column(db.DateTime,default=datetime.utcnow)
-
+    data = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -27,7 +27,6 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route('/about')
 def about():
     return render_template("about.html")
@@ -37,31 +36,25 @@ def about():
 def newpage():
     return render_template("newpage.html")
 
-@app.route('/create_article',methods=['POST','GET'])
+
+@app.route('/create_article', methods=['POST', 'GET'])
 def create_article():
-    if request.method=="POST":
+    if request.method == "POST":
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
 
-
-        article = Article(title=title,intro=intro,text=text)
+        article = Article(title=title, intro=intro, text=text)
 
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect(url_for('index'))
         except:
-            return "При добавлении статьи произошла ошибка"
+            return " При добавлении статьи произошла ошибка "
     else:
         return render_template("create_article.html")
 
-#@app.route('/user/<string:name>/<int:id>')
-#def user(name,id):
- #   return 'User page'+name+"-"+ str(id)
-
 
 if __name__ == '__main__':
-   app.run(debug=True)
-
-
+    app.run(debug=True)
